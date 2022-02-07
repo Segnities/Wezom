a14a57ef23c7a3c38e68fd78130132676dc7bda2
Segnities <dimab787898@gmail.com>
2022-02-07 16:49:43 +0200

    Супер-мега-дупер готовый слайдер

--- src/assets/js/slider.js
+++ src/assets/js/slider.js
@@ -2,10 +2,21 @@ let slideIndex = 1;
 
 class Slider{
     showSlides(index){
+        let mySlides = document.getElementsByClassName('main-product-img');
         let slides = document.getElementsByClassName('product-scroller-img');
+        if(index>mySlides.length){
+            slideIndex = 1;
+        }
+        if(index<1){
+            slideIndex = mySlides.length
+        }
+        for(let i = 0; i<mySlides.length; i++){
+            mySlides[i].style.display = 'none';
+        }
         for(let i = 0; i<slides.length;i++){
             slides[i].className = slides[i].className.replace(' active-product-scroller','');
         }
+        mySlides[slideIndex].style.display = 'block';
         slides[slideIndex-1].className += ' active-product-scroller';
     }
     currentSlide(index){
